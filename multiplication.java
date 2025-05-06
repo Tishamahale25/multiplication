9b93c0084dda7545772fe9e990b1caf059ca2ae9
Tishamahale25 <Tishamahale25@gmail.com>
2025-05-06 16:14:58 +0530

    save master

--- multiplication.java
+++ multiplication.java
@@ -2,7 +2,8 @@ public class multiplication {
     public static void main(String[] args) {
         int num1 = 5;
         int num2 = 4;
-        int result = num1 * num2;
+        int num3 = 6;
+        int result = num1 * num2 * num3;
 
         System.out.println("Multiplication of " + num1 + " and " + num2 + " is: " + result);
     }
