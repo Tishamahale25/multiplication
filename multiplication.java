610e5c479ab880b94606f393a34bab7e1656fdd2
Tishamahale25 <Tishamahale25@gmail.com>
2025-05-06 16:11:30 +0530

    save

---
+++ multiplication.java
@@ -0,0 +1,9 @@
+public class multiplication {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 4;
+        int result = num1 * num2;
+
+        System.out.println("Multiplication of " + num1 + " and " + num2 + " is: " + result);
+    }
+}
